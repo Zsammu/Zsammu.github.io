60db57bcdb75c697eef3b66e8e891cb23d5a5b6a
Ahri <154325611+Zsammu@users.noreply.github.com>
2025-02-05 14:25:20 +0800

    Add files via upload

---
+++ Tsa2/Tsa2.js
@@ -0,0 +1,30 @@
+var box = document.getElementById("boxColor");
+
+function MondayBox(){
+    box.style.border = "5px solid rgb(0, 0, 0)";
+    box.style.transform = translateY("2px");
+}
+
+function TuesdayBox(){
+    box.style.border = "5px solid rgb(0, 0, 0)";
+}
+
+function WednesdayBox(){
+    box.style.border = "5px solid rgb(0, 0, 0)";
+}
+
+function ThursdayBox(){
+    box.style.border = "5px solid rgb(0, 0, 0)";
+}
+
+function FridayBox(){
+    box.style.border = "5px solid rgb(0, 0, 0)";
+}
+
+function SaturdayBox(){
+    box.style.border = "5px solid rgb(0, 0, 0)";
+}
+
+function SundayBox(){
+    box.style.border = "5px solid rgb(0, 0, 0)";
+}
