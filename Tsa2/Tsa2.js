5f5e1c6f3c76e362bb13aba7686230e5a4128687
Ahri <154325611+Zsammu@users.noreply.github.com>
2025-02-11 09:35:53 +0800

    Add files via upload

---
+++ Tsa2/Tsa2.js
@@ -0,0 +1,146 @@
+function boxButton1() {
+    let box1 = document.getElementsByClassName("box1")[0];
+    let box2 = document.getElementsByClassName("box2")[0];
+    let box3 = document.getElementsByClassName("box3")[0];
+    let box4 = document.getElementsByClassName("box4")[0];
+    let box5 = document.getElementsByClassName("box5")[0];
+    let box6 = document.getElementsByClassName("box6")[0];
+    let box7 = document.getElementsByClassName("box7")[0];
+
+    // Ensure elements exist before modifying styles
+    if (box1 && box2 && box3 && box4 && box5 && box6 && box7) {
+        box1.style.transform = "translateY(0px)";
+        box2.style.transform = "translateY(-1000px)";
+        box3.style.transform = "translateY(-1000px)";
+        box4.style.transform = "translateY(-1000px)";
+        box5.style.transform = "translateY(-1000px)";
+        box6.style.transform = "translateY(-1000px)";
+        box7.style.transform = "translateY(-1000px)";
+    }
+}
+
+function boxButton2() {
+    var box1 = document.getElementsByClassName("box1")[0]; // Access the first element
+    var box2 = document.getElementsByClassName("box2")[0];
+    var box3 = document.getElementsByClassName("box3")[0];
+    var box4 = document.getElementsByClassName("box4")[0];
+    var box5 = document.getElementsByClassName("box5")[0];
+    var box6 = document.getElementsByClassName("box6")[0];
+    var box7 = document.getElementsByClassName("box7")[0];
+
+    // Now you can modify the styles
+    if (box1 && box2 && box3 && box4 && box5 && box6 && box7){
+        box1.style.transform = "translateY(-1000px)";
+        box2.style.transform = "translateY(0px)";
+        box3.style.transform = "translateY(-1000px)";
+        box4.style.transform = "translateY(-1000px)";
+        box5.style.transform = "translateY(-1000px)";
+        box6.style.transform = "translateY(-1000px)";
+        box7.style.transform = "translateY(-1000px)";
+    }
+}
+
+function boxButton3() {
+    var box1 = document.getElementsByClassName("box1")[0]; // Access the first element
+    var box2 = document.getElementsByClassName("box2")[0];
+    var box3 = document.getElementsByClassName("box3")[0];
+    var box4 = document.getElementsByClassName("box4")[0];
+    var box5 = document.getElementsByClassName("box5")[0];
+    var box6 = document.getElementsByClassName("box6")[0];
+    var box7 = document.getElementsByClassName("box7")[0];
+
+    // Now you can modify the styles
+    if (box1 && box2 && box3 && box4 && box5 && box6 && box7){
+    box1.style.transform = "translateY(-1000px)";
+    box2.style.transform = "translateY(-1000px)";
+    box3.style.transform = "translateY(0px)";
+    box4.style.transform = "translateY(-1000px)";
+    box5.style.transform = "translateY(-1000px)";
+    box6.style.transform = "translateY(-1000px)";
+    box7.style.transform = "translateY(-1000px)";
+    }
+}
+
+function boxButton4() {
+    var box1 = document.getElementsByClassName("box1")[0]; // Access the first element
+    var box2 = document.getElementsByClassName("box2")[0];
+    var box3 = document.getElementsByClassName("box3")[0];
+    var box4 = document.getElementsByClassName("box4")[0];
+    var box5 = document.getElementsByClassName("box5")[0];
+    var box6 = document.getElementsByClassName("box6")[0];
+    var box7 = document.getElementsByClassName("box7")[0];
+
+    // Now you can modify the styles
+    if (box1 && box2 && box3 && box4 && box5 && box6 && box7){
+    box1.style.transform = "translateY(-1000px)";
+    box2.style.transform = "translateY(-1000px)";
+    box3.style.transform = "translateY(-1000px)";
+    box4.style.transform = "translateY(0px)";
+    box5.style.transform = "translateY(-1000px)";
+    box6.style.transform = "translateY(-1000px)";
+    box7.style.transform = "translateY(-1000px)";
+    }
+}
+
+function boxButton5() {
+    var box1 = document.getElementsByClassName("box1")[0]; // Access the first element
+    var box2 = document.getElementsByClassName("box2")[0];
+    var box3 = document.getElementsByClassName("box3")[0];
+    var box4 = document.getElementsByClassName("box4")[0];
+    var box5 = document.getElementsByClassName("box5")[0];
+    var box6 = document.getElementsByClassName("box6")[0];
+    var box7 = document.getElementsByClassName("box7")[0];
+
+    // Now you can modify the styles
+    if (box1 && box2 && box3 && box4 && box5 && box6 && box7){
+    box1.style.transform = "translateY(-1000px)";
+    box2.style.transform = "translateY(-1000px)";
+    box3.style.transform = "translateY(-1000px)";
+    box4.style.transform = "translateY(-1000px)";
+    box5.style.transform = "translateY(0px)";
+    box6.style.transform = "translateY(-1000px)";
+    box7.style.transform = "translateY(-1000px)";
+    }
+}
+
+function boxButton6() {
+    var box1 = document.getElementsByClassName("box1")[0]; // Access the first element
+    var box2 = document.getElementsByClassName("box2")[0];
+    var box3 = document.getElementsByClassName("box3")[0];
+    var box4 = document.getElementsByClassName("box4")[0];
+    var box5 = document.getElementsByClassName("box5")[0];
+    var box6 = document.getElementsByClassName("box6")[0];
+    var box7 = document.getElementsByClassName("box7")[0];
+
+    // Now you can modify the styles
+    if (box1 && box2 && box3 && box4 && box5 && box6 && box7){
+    box1.style.transform = "translateY(-1000px)";
+    box2.style.transform = "translateY(-1000px)";
+    box3.style.transform = "translateY(-1000px)";
+    box4.style.transform = "translateY(-1000px)";
+    box5.style.transform = "translateY(-1000px)";
+    box6.style.transform = "translateY(0px)";
+    box7.style.transform = "translateY(-1000px)";
+    }
+}
+
+function boxButton7() {
+    var box1 = document.getElementsByClassName("box1")[0]; // Access the first element
+    var box2 = document.getElementsByClassName("box2")[0];
+    var box3 = document.getElementsByClassName("box3")[0];
+    var box4 = document.getElementsByClassName("box4")[0];
+    var box5 = document.getElementsByClassName("box5")[0];
+    var box6 = document.getElementsByClassName("box6")[0];
+    var box7 = document.getElementsByClassName("box7")[0];
+
+    // Now you can modify the styles
+    if (box1 && box2 && box3 && box4 && box5 && box6 && box7){
+    box1.style.transform = "translateY(-1000px)";
+    box2.style.transform = "translateY(-1000px)";
+    box3.style.transform = "translateY(-1000px)";
+    box4.style.transform = "translateY(-1000px)";
+    box5.style.transform = "translateY(-1000px)";
+    box6.style.transform = "translateY(-1000px)";
+    box7.style.transform = "translateY(0px)";
+    }
+}
